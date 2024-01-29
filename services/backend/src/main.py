d146810680046f14424c164caf4251a8dabe4c24
abroberts14 <36211649+abroberts14@users.noreply.github.com>
2024-01-29 17:26:10 -0500

    update ports to fix cors error

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
