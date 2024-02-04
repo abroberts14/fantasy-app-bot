ed68d5b55827c9b6004ededb86da2790a1a3e115
bsalvaggio <119374432+bsalvaggio@users.noreply.github.com>
2024-02-03 21:26:22 -0500

    frontend-logo

--- services/frontend/src/views/HomeView.vue
+++ services/frontend/src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
   <section>
-    <p>This site is built with FastAPI and Vue.</p>
+    <center>
+    <h1 style="font-size: 48px; font-weight: bold; font-family: 'Arial', sans-serif;">Welcome to SRC Bot!</h1> <br>
+    <img src="@/assets/srclogo1.png" alt="SRC Bot" 
+        width="400" height="400"> <br><br>
 
+    <br>
     <div v-if="isLoggedIn" id="logout">
       <p id="logout">Click <a href="/dashboard">here</a> to view all notes.</p>
     </div>
-    <p v-else>
+    <h2 v-else>
       <span><a href="/register">Register</a></span>
       <span> or </span>
       <span><a href="/login">Log In</a></span>
-    </p>
+    </h2>
+    </center>
   </section>
 </template>
 <script>
