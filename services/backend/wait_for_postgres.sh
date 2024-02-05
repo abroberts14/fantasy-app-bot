22e95d2dd2c572a9bc201e88ea11071d071ea755
bsalvaggio <119374432+bsalvaggio@users.noreply.github.com>
2024-02-05 01:59:42 -0500

    rds and pgadmin
    configured rds to work, pgadmin, and env

--- services/backend/wait_for_postgres.sh
+++ services/backend/wait_for_postgres.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# The RDS endpoint should be passed as the first argument
 host="$1"
 shift
 cmd="$@"
@@ -10,7 +11,9 @@ cmd="$@"
 timeout=15
 count=0
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+# Attempt to connect to the PostgreSQL database using provided credentials
+# If the connection fails, sleep for 1 second and try again
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
   count=$((count + 1))
@@ -20,5 +23,31 @@ until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
   fi
 done
 
->&2 echo "Postgres is up - executing command update "
+>&2 echo "Postgres is up - executing command"
 eval $cmd
+
+
+# #!/bin/bash
+
+# set -e
+
+# host="$1"
+# shift
+# cmd="$@"
+
+# # Timeout after 15 seconds
+# timeout=15
+# count=0
+
+# until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+#   >&2 echo "Postgres is unavailable - sleeping"
+#   sleep 1
+#   count=$((count + 1))
+#   if [ $count -ge $timeout ]; then
+#     >&2 echo "Timeout reached, exiting"
+#     exit 1
+#   fi
+# done
+
+# >&2 echo "Postgres is up - executing command update "
+# eval $cmd
