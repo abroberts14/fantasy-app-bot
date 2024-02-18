19a3a749da6f68b84b107cf63a99b1c5c86c62ca
abroberts14 <36211649+abroberts14@users.noreply.github.com>
2024-02-18 12:47:32 -0500

    fixes

--- services/baseball_bot/utils/scheduler.py
+++ services/baseball_bot/utils/scheduler.py
@@ -9,7 +9,7 @@ def scheduler():
     # sched.add_job(yahoo_bot, 'cron', ['get_league_team_names'], id='team_names',
     #               day_of_week='mon', hour=18, minute=30, start_date=ff_start_date, end_date=ff_end_date,
     #               timezone=data['bot_timezone'], replace_existing=True)
-    sched.add_job(yahoo_bot, 'interval', minutes=data['team_names_minutes'], args=['get_league_team_names'], id='team_names',
+    sched.add_job(yahoo_bot, 'interval', minutes=int(data['team_names_minutes']), args=['get_league_team_names'], id='team_names',
               timezone=data['bot_timezone'], replace_existing=True)
     print("Ready!")
     sched.start()
