bbf780a43757b41d5617ce4514d526765435b218
abroberts14 <36211649+abroberts14@users.noreply.github.com>
2024-05-09 10:42:37 -0400

    fix vite

--- services/frontend/vite.config.js
+++ services/frontend/vite.config.js
@@ -2,21 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import createPersistedState from 'pinia-plugin-persistedstate'
-import { vite as vidstack } from 'vidstack/plugins';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('media-'),
-        },
-      },
-    }),
-    vidstack(),
-
+    vue()
   ],
   resolve: {
     alias: {
@@ -31,4 +21,3 @@ export default defineConfig({
     }
   }
 })
-
